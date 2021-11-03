4ad9e3562b34151c73e8bd9c302d3523d89c2623
hodan33 <93352028+hodan33@users.noreply.github.com>
2021-11-03 21:40:50 +0200

    Create Untitled3.c

---
+++ Untitled3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int first_num = 8;
+float sec_num = 3.5;
+char chrarcter ='a';
+int main (){
+	char array[20]="hello c lang";
+
+	printf("%d is an intger\n ", first_num);
+	printf("%f is a decimal \n ", sec_num);
+	printf("%c is a letter\n ", chrarcter);
+	printf("%s ", array);
+	return 0;
+}
+
